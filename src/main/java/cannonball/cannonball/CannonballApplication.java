f3d6e8273b5289e667b04407f70c057b23e7eba5
SUNMI-KIM <hariaus@naver.com>
2023-05-15 22:21:34 +0900

    초기 파일

---
+++ src/main/java/cannonball/cannonball/CannonballApplication.java
@@ -0,0 +1,13 @@
+package cannonball.cannonball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CannonballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CannonballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cannonball/cannonball/CannonballApplicationTests.java
@@ -0,0 +1,13 @@
+package cannonball.cannonball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CannonballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
